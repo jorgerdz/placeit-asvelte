793b604957e6825c8a1744e0f24d2bc11e38248c
Jorge Rodriguez <jorgecrb@gmail.com>
2021-09-02 00:55:29 -0500

    Adding Tags

---
+++ src/components/Tags.svelte
@@ -0,0 +1,8 @@
+<title style="box-sizing: border-box;">Parcel Sandbox</title>
+<meta charset="UTF-8" style="box-sizing: border-box;">
+<meta http-equiv="X-UA-Compatible" content="IE=edge" style="box-sizing: border-box;">
+<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1" style="box-sizing: border-box;">
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,600,700&amp;display=swap" rel="preload"
+ as="style" onload="this.onload=null;this.rel='stylesheet'" style="box-sizing: border-box;">
+<link href="https://fonts.googleapis.com/css?family=Hind:300,400,500|Poppins:400,500,600,700&amp;display=swap" rel="preload"
+ as="style" onload="this.onload=null;this.rel='stylesheet'" style="box-sizing: border-box;">
